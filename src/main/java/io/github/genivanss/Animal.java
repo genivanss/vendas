a26d3eeb70b1ce83abaa801f36acc93ed522fa53
genivanss <genivanss@yahoo.com.br>
2023-01-30 20:31:46 -0300

    Seção 03 - Projeto de Vendas

--- src/main/java/io/github/genivanss/Animal.java
+++
@@ -1,5 +0,0 @@
-package io.github.genivanss;
-
-public interface Animal {
-    void fazerBarulho();
-}
--- src/main/java/io/github/genivanss/VendasApplication.java
+++ src/main/java/io/github/genivanss/VendasApplication.java
@@ -1,44 +1,10 @@
 package io.github.genivanss;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {
-    "io.github.genivanss",
-    "io.github.genivanss.repository",
-    "org.springframework.stereotype.Service;"}
-    )
-@RestController
 public class VendasApplication {
-
-    //@Autowired
-    //@Qualifier("applicationName")
-    @Value("${application.name}")
-    private String applicationName;
-
-    @Cachorro
-    private Animal animal;
-
-    @Bean(name = "executarAnimal")
-    public CommandLineRunner executar(){
-        return args -> {
-            this.animal.fazerBarulho();
-        };
-    }
-
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return applicationName;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
